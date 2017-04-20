42d86987e5f3a7fb211120ba16c0446566da0e80
sanzenwin <sanzenwin@gmail.com>
2017-04-21 06:46:16 +0800

    up

--- common/plugins/apps/default/plugins/setup.py
+++ common/plugins/apps/default/plugins/setup.py
@@ -11,19 +11,29 @@ def init_ret_code(plugins):
     template_str = '# coding:utf-8\r\n"""\r\n{\r\n%s\r\n}\r\n"""\r\n\r\n\r\n%s\r\n'
     client_dict = OrderedDict()
     server_dict = OrderedDict()
-    for i, c in enumerate(reversed(settings.RetCode.__class__.mro())):
-        for j, k in enumerate(sorted(c.__dict__)):
+    i = 1
+    for c in reversed(settings.RetCode.__class__.mro()):
+        b = len(client_dict)
+        j = 1
+        for k in sorted(c.__dict__):
             v = c.__dict__[k]
             if isinstance(v, Str):
-                code = (i + 1) * 100 + j
+                code = i * 100 + j
+                if k in server_dict:
+                    client_dict.pop(server_dict[k])
                 client_dict[code] = v
                 server_dict[k] = code
+                j += 1
+        if b != len(client_dict):
+            i += 1
+
     client_list = []
     for k, v in client_dict.items():
         client_list.append('    %s: "%s"' % (k, v))
     server_list = []
     for k, v in server_dict.items():
         server_list.append("%s = %s" % (k, v))
+        client_list.append('    "%s": %s' % (k, v))
 
     s = template_str % (",\r\n".join(client_list), "\r\n".join(server_list))
     plugins.write(s, plugins.PLUGINS_PROXY_COMMON_DIR, "ret_code.py")
