debe23fa4ce32768cb6b44f156b5045bf7a6c64d
sanzenwin <sanzenwin@gmail.com>
2017-08-31 20:26:03 +0800

    up

--- common/kbe/log.py
+++ common/kbe/log.py
@@ -37,6 +37,26 @@ def SHOW_MSG(*args):
     ERROR_MSG(*args)
 
 
+class Log:
+    tag_template = "[{0}]"
+
+    def __init__(self, tag):
+        self.tag = tag
+        self.__tag = self.tag_template.format(self.tag)
+
+    def d(self, s):
+        DEBUG_MSG("%s %s" % (self.__tag, s))
+
+    def i(self, s):
+        INFO_MSG("%s %s" % (self.__tag, s))
+
+    def w(self, s):
+        WARNING_MSG("%s %s" % (self.__tag, s))
+
+    def e(self, s):
+        ERROR_MSG("%s %s" % (self.__tag, s))
+
+
 def _print(*args, sep=' ', end='\n', file=None):
     ERROR_MSG(*args)
 
