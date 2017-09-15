5f1734867cd9d1cf166eaa29b7cd8a5695ab1980
sanzenwin <sanzenwin@gmail.com>
2017-09-15 19:18:51 +0800

    up

--- common/kbe/log.py
+++ common/kbe/log.py
@@ -38,27 +38,32 @@ def SHOW_MSG(*args):
 
 
 class Log:
-    tag_template = "[{0}]"
+    tag_template = "[{tag}]"
+    msg_template = "{tag} {msg}"
 
     def __init__(self, tag):
         self.tag = tag
-        self.__tag = self.tag_template.format(self.tag)
+        self.__tag = self.tag_template.format(tag=self.tag)
+
+    def get_msg(self, t, s):
+        return self.msg_template.format(tag=self.__tag, msg=s)
 
     def d(self, s):
-        DEBUG_MSG("%s %s" % (self.__tag, s))
+        DEBUG_MSG(self.get_msg("debug", s))
 
     def i(self, s):
-        INFO_MSG("%s %s" % (self.__tag, s))
+        INFO_MSG(self.get_msg("info", s))
 
     def w(self, s):
-        WARNING_MSG("%s %s" % (self.__tag, s))
+        WARNING_MSG(self.get_msg("warning", s))
 
     def e(self, s):
-        ERROR_MSG("%s %s" % (self.__tag, s))
+        ERROR_MSG(self.get_msg("error", s))
 
 
 def _print(*args, sep=' ', end='\n', file=None):
     ERROR_MSG(*args)
+
 
 __print__ = print
 builtins.print = _print
