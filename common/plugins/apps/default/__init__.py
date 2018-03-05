d8e2b37ac34f1f7fba6f35781692085da641b06d
sanzenwin <sanzenwin@gmail.com>
2018-03-06 03:03:50 +0800

    up

--- common/plugins/apps/default/__init__.py
+++ common/plugins/apps/default/__init__.py
@@ -1,6 +1,6 @@
 __third_package__ = (
-    "xlrd", "tornado", "yapf", "aioredis==0.3.5", "redis", "pymongo==3.5.0", "pymysql", "motor==1.1", "dicttoxml",
-    "oscrypto", "simplejson", "sortedcontainers"
+    "xlrd", "tornado==4.5.2", "yapf", "aioredis==0.3.5", "redis", "pymongo==3.5.0", "pymysql", "motor==1.1",
+    "dicttoxml", "oscrypto", "simplejson", "sortedcontainers"
 )
 
 __kbengine_xml_default__ = dict(
